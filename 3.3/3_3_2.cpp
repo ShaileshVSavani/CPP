d4857d585f28769298d9f209e7e6effdfeaa23df
ShaileshVSavani <svs.rnw23@gmail.com>
2023-09-02 09:21:12 +0530

    delete

--- 3.3/3_3_2.cpp
+++
@@ -1,7 +0,0 @@
-/*Q.2 Write a Program to get and display N numbers of Fast Food cafe information using encapsulation and the use of a Default constructor by including the below-mentioned attributes:- cafe_id
-- cafe_name
-- cafe_type (like a rooftop or normal)
-- cafe_rating (like 1 Star, 2 Start, ..., 5 Star)
-- cafe_location (city name)
-- cafe_establish_year
-- cafe_staff_quantity*/
