6e48721526deb34d77f1344198f5db3be9c590c9
ShaileshVSavani <svs.rnw23@gmail.com>
2023-09-05 19:15:04 +0530

    05/09/2023

---
+++ 2.1/2_1_1.cpp
@@ -0,0 +1,10 @@
+/*Q.1 Write a Program to create an Employee Record System using the Class & Object of any 5 employees. Consider the below-mentioned attributes in the Employee class:
+- emp_id
+- emp_name
+- emp_age
+- emp_role
+- emp_salary
+- emp_city
+- emp_experience
+- emp_company_name*/
+
